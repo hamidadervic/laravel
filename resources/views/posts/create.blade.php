b1d8a8d95f2fe30845c5e548f46e6b3c695a1bf5
Hamida <hamida.dervic@outlook.com>
2017-10-30 13:24:09 +0100

    create post HTML form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,6 +5,17 @@
          <div class="row">
             <div class="jumbotron col-md-6 offset-md-3">
                 <h4 class="text-center"> Create post </h4>
+                <?php echo Form::open(['action' => 'PostsController@store', 'method' => 'POST']); ?>
+                  <div class="form-group">
+                      <?php echo Form::label('title','Title'); ?>
+                      <?php echo Form::text('title','',['class' => 'form-control','placeholder' => 'Add your title']); ?>
+                  </div>
+                  <div class="form-group">
+                      <?php echo Form::label('body','Body'); ?>
+                      <?php echo Form::textarea('body','',['class' => 'form-control','placeholder' => 'Add your title', 'coll' => 3]); ?>
+                  </div>
+                  <?php echo Form::submit('Submit',['class' => 'btn btn-default']); ?>
+                <?php echo Form::close(); ?>
              </div>
          </div>
     </div>
