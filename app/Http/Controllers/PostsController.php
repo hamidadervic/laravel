780a7136dd35b0c25d155004fa9032a5e89034c0
Hamida <hamida.dervic@outlook.com>
2017-10-31 21:56:18 +0100

    image upload problem

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -49,14 +49,38 @@ class PostsController extends Controller
         //Validation of form data
         $this->validate($request, [
                  'title' => 'required',
-                 'body' => ' required'
+                 'body' => ' required',
+                 'coverimage' => 'image|max:1999',
             ]);
+
+        if( $request->hasFile('coverimage') ) {
+           
+            $fileNameWithExtension = $request->file('coverimage')->getClientOriginalName();
+
+            // There could be a problem if user upload an image with a name that already exists
+
+            // Get just file name
+            $fileName = pathinfo($fileNameWithExtension, PATHINFO_FILENAME);
+
+            //Get just EXTENSION
+            $extension = $request->file('coverimage')->getClientOriginalExtension();
+
+            // And now the full name of image, we will use the time() function so name of every image will be unique
+
+            $fileNameToStore = $fileName . '_' . time() . '.' . $extension; 
+
+            // On the end, the variable for saving the image with laravel
+            $path = $request->file('coverimage')->storeAs('public/coverimages',$fileNameToStore); //Hm I can't use storeAs because of version - 5-2-45
+        } else {
+            $fileNameToStore = 'noimage';
+        }
 
         //Create new post based on inputs
         $post = new Post; //We can use it 'cause we have it on the top
+        $post->user_id = auth()->user()->id;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
-        $post->user_id = auth()->user()->id;
+        $post->coverimage = $fileNameToStore;
         $post->save();
 
         return redirect('/posts')->with('success','New post created!');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
          <div class="row">
             <div class="jumbotron col-md-8 offset-md-2">
                 <h4 class="text-center"> Create post </h4>
-                <?php echo Form::open(['action' => 'PostsController@store', 'method' => 'POST']); ?>
+                <?php echo Form::open(['action' => 'PostsController@store', 'method' => 'POST','enctype' => 'multipart/form-data']); ?>
                   <div class="form-group">
                       <?php echo Form::label('title','Title'); ?>
                       <?php echo Form::text('title','',['class' => 'form-control','placeholder' => 'Add your title']); ?>
@@ -13,6 +13,10 @@
                   <div class="form-group">
                       <?php echo Form::label('body','Body'); ?>
                       <?php echo Form::textarea('body','',['id' => 'article-ckeditor','class' => 'form-control','placeholder' => 'Add your body text', 'coll' => 3]); ?>
+                  </div>
+                  <div class="form-group">
+                      <?php echo Form::file('coverimage') ?>
+                     
                   </div>
                   <?php echo Form::submit('Submit',['class' => 'btn btn-default']); ?>
                 <?php echo Form::close(); ?>
